87705f62bce2009a4e8f96c288c8b01bbc6a66b9
Kirito-ENGR <buivietha12012003@gmail.com>
2025-11-03 17:13:52 +0700

    Note make by Bui Viet Ha

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,15 @@ class MyApp extends StatefulWidget{
 class _MyAppState extends State<MyApp> {
   final List<DuLieu> items = [];
 
+  void SuaGhiChu(String id, String newName) {
+  setState(() {
+    final index = items.indexWhere((item) => item.id == id);
+    if (index != -1) {
+      items[index] = DuLieu(id: id, name: newName);
+    }
+  });
+}
+
   void ThemGhiChu(String name){
     final newItem = DuLieu(id: DateTime.now().toString(),name: name);
     setState((){
@@ -33,7 +42,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context){
     return Scaffold (
-      backgroundColor: const Color.fromARGB(255, 143, 69, 32),
+      backgroundColor: const Color.fromARGB(255, 190, 244, 188),
       appBar: AppBar(
         centerTitle: true,
         title: const Text(
@@ -46,7 +55,9 @@ class _MyAppState extends State<MyApp> {
       body: SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 20,vertical:20),
         child: Column(
-          children: items.map((item) => GhiChuMoi(item : item, xoa: XoaGhiChu,)).toList(),
+          children: items.map((item) => GhiChuMoi(
+            index : items.indexOf(item),
+            item : item, xoa: XoaGhiChu,sua: SuaGhiChu,)).toList(),
         ),
       ),
       floatingActionButton : FloatingActionButton(
@@ -60,8 +71,10 @@ class _MyAppState extends State<MyApp> {
             },
           );
         },
-        child: Icon(Icons.add,size: 60,color: Colors.orange,),
-      )
+        child: Center(
+          child: Icon(Icons.add,size: 50,color: Colors.orange,),
+        ),
+     )
     );
   }
 }
@@ -117,8 +130,10 @@ class NhapGhiChu extends StatelessWidget {
   }
 }
 class GhiChuMoi extends StatelessWidget {
-  GhiChuMoi({Key? key,required this.item, required this.xoa}) : super(key:key);
+  GhiChuMoi({Key? key,required this.item, required this.xoa,required this.index,required this.sua,}) : super(key:key);
   var item;
+  var index;
+  var sua;
   final Function xoa;
   @override
   Widget build(BuildContext context) {
@@ -127,7 +142,9 @@ class GhiChuMoi extends StatelessWidget {
       height: 60,
       margin: EdgeInsets.only(bottom: 20),
       decoration:BoxDecoration(
-        color: Colors.amber,
+        color: (index%2==0)
+          ? const Color.fromARGB(255, 85, 172, 252)
+          : const Color.fromARGB(222, 251, 76, 76),
         borderRadius:BorderRadius.circular(12),
       ),
       child: Padding(
@@ -139,22 +156,43 @@ class GhiChuMoi extends StatelessWidget {
               item.name,
               style: const TextStyle(
                 fontSize: 20,
-                color: Colors.red,
+                color: Color.fromARGB(255, 0, 0, 0),
               )
             ),
-            InkWell(
-              onTap:() async {
-                if(await confirm(context)){
-                  xoa(item.id);
-                }
-                return;
-
-              },
-              child: const Icon(
-                Icons.delete_forever_outlined,
-                color: Colors.black54,
-                size: 40,
-              ),
+            Row(
+              children: [
+                InkWell(
+                  onTap: () async {
+                    final newText = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ChinhSua(item: item),
+                    ),
+                    );
+                    if (newText != null && newText is String && newText.isNotEmpty) {
+                      sua(item.id, newText);
+                    }
+                  },
+                  child: const Icon(
+                    Icons.edit,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    size: 40,
+                  ),
+                ),
+                InkWell(
+                  onTap:() async {
+                    if(await confirm(context)){
+                      xoa(item.id);
+                    }
+                    return;
+                  },
+                  child: const Icon(
+                    Icons.delete_forever_outlined,
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    size: 40,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -172,3 +210,46 @@ class DuLieu{
     required this.name
   });
 }
+
+class ChinhSua extends StatelessWidget {
+  final DuLieu item;
+  const ChinhSua({Key? key, required this.item}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController controller =
+        TextEditingController(text: item.name); // Ghi chú cũ hiển thị sẵn
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chỉnh sửa ghi chú'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Sửa nội dung ghi chú',
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context, controller.text);
+                },
+                child: const Text('Lưu thay đổi'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
